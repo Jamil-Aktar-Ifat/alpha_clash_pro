84303920f184d38f31c373993dd6bcbd58547371
ifat <i>
2024-04-22 14:42:31 +0100

    functionality updated

--- scripts/alpha_clash.js
+++ scripts/alpha_clash.js
@@ -7,10 +7,34 @@
 //   playgroundSection.classList.remove("hidden");
 // }
 
+function handleKeyboardButtonPress(event) {
+  const playerPressed = event.key;
+  // console.log("Player pressed: ", playerPressed);
+
+  // get the expected to press
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLowerCase();
+  // console.log(playerPressed, expectedAlphabet);
+
+  // check if matched or not
+  if (playerPressed === expectedAlphabet) {
+    console.log("You got a point!");
+    console.log("You have pressed correctly", expectedAlphabet);
+    removeBackgroundColorById(expectedAlphabet);
+    continueGame();
+  } else {
+    console.log("You missed a life!");
+  }
+}
+
+// keyboard key press
+document.addEventListener("keyup", handleKeyboardButtonPress);
+
 function continueGame() {
   // step - 1: generate a random alphabet
   const alphabet = getRandomAlphabet();
-  console.log("Your random alphabet:", alphabet);
+  // console.log("Your random alphabet:", alphabet);
 
   //   set randomly generated alphabet
   const currentAlphabetElement = document.getElementById("current-alphabet");
